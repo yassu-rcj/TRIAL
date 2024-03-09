067f7b2b576edb774feca9bca001b278fb1dd29f
yassu-rcj <m2207146@ritsumori.ed.jp>
2024-03-09 11:35:33 +0900

    a

--- lib/ball.cpp
+++ lib/ball.cpp
@@ -1,29 +1,40 @@
 #include <Arduino.h>
 #define pin 8
-int b,c;
+int b, c;
 unsigned long time_new;
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  pinMode(pin,INPUT);
+  pinMode(pin, INPUT);
 }
-void loop(){   
- time_new=micros();
- b = digitalRead(8);
- if (time_new % 833 == 0){
-if (time_new==0){
-     c=1;
-}else{
-  Serial.print(">c:");
-  Serial.println(c);
-  Serial.print(">b:");
-  Serial.println(b);
- c=0;
-}
- }else{
-  if (b==0){
-    c=c;
-  }else{
-    c=c+1;
- }
- }
+void loop()
+{
+  time_new = micros();
+  b = digitalRead(8);
+  if (time_new % 833 == 0)
+  {
+    if (time_new == 0)
+    {
+      c = 1;
+    }
+    else
+    {
+      Serial.print(">c:");
+      Serial.println(c);
+      Serial.print(">b:");
+      Serial.println(b);
+      c = 0;
+    }
+  }
+  else
+  {
+    if (b == 0)
+    {
+      c = c;
+    }
+    else
+    {
+      c = c + 1;
+    }
+  }
 }
